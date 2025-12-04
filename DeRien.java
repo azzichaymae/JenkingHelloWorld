6523d3e316da6e3725dbc48d63a4f0b2391b3adb
azzichaymae <142222368+azzichaymae@users.noreply.github.com>
2025-12-04 19:46:02 +0000

    test

---
+++ DeRien.java
@@ -0,0 +1,6 @@
+class DeRien(){
+    public static void main(String[] args) {
+    
+    System.out.println("De Rien"); 
+    }
+}
---
+++ Merci.java
@@ -0,0 +1,6 @@
+class Merci(){
+    public static void main(String[] args) {
+    
+    System.out.println("Merci."); 
+    }
+}
